5d6a7986736ecf4931df14382b4586771a1d3a6c
Vlad <zeulnimeni@gmail.com>
2023-08-04 20:26:15 +0300

    add most of the home dom elements and hamburger icons

---
+++ src/home.js
@@ -0,0 +1,23 @@
+
+
+function createHeader()
+{
+  const headArray=['Home','Contact','About']
+    const header=document.createElement('header');
+    for(let i=0;i<=2;i++)
+    {
+        let ase=document.createElement('p');
+        ase.textContent=headArray[i]
+       header.appendChild(ase);    
+    }
+  return header;
+}
+
+function createFooter()
+{
+    const footer=document.createElement('footer');
+    footer.textContent="Odin project";
+    return footer;
+}
+
+export {createHeader,createFooter};
--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+import { createHeader,createFooter } from "./home"
+
+const content=document.querySelector('#content');
+
+content.appendChild(createHeader())
+content.appendChild(createFooter())
+
 console.log('I am working')
